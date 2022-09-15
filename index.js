2891f9950c2395fa67af4358459709cf71bf64ad
Fleeym <gabi.badea313@gmail.com>
2022-09-15 23:37:35 +0300

    i did stuff

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 'use strict';
+
+const screen = document.getElementById('screen');
+
+let lastOpperand = '';
+let memory = 0;
+let x = '';
+
+function inputCharacter(character) {
+    if (character === '.') {
+        // logic for floating point
+    } else {
+        x += character;
+        screen.value = x;
+    }
+}
+
+function calculate() {}
+
+function operate(operator) {
+    if (x === '') {
+        return;
+    }
+    if (lastOpperand === '') {
+        switch (operator) {
+            case '+':
+                lastOpperand = '+';
+                memory += x;
+                break;
+            case '-':
+                lastOpperand = '-';
+                memory += x;
+                break;
+            case '*':
+                lastOpperand = '*';
+                memory += x;
+                break;
+            case '/':
+                lastOpperand = '/';
+                memory += x;
+                break;
+        }
+        screen.value = '';
+        console.log('current memory', memory);
+    }
+}
+
+function reset(type) {
+    switch (type) {
+        case 'soft':
+            x = '';
+            screen.value = '';
+            break;
+        case 'hard':
+            x = '';
+            lastOpperand = '';
+            screen.value = '';
+            break;
+        default:
+            alert('Something bad happened!');
+            break;
+    }
+}
